6c4bf1250fe28608408cd7739116d5092a881da5
Alexandr <semakaleksandr2014@gmail.com>
2018-06-01 21:14:16 +0300

    added allContacts

---
+++ code/js/components/allContact.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+class AllContact extends React.Component {
+
+  render() {
+      return (  
+
+				<div className="two column centered row">
+
+			    <div className="column">
+
+		    		<div className="ui large animated list">
+						  <div className="item">
+						    <img className="ui avatar image" src="https://semantic-ui.com/images/avatar/large/christian.jpg" />
+						    <div className="content">
+						      <a className="header">Александр Семак</a>
+						      <div className="description">+380997708164</div>
+						    </div>
+						  </div>
+						</div>
+			    </div>
+
+			    <div className="column">
+			    	
+			    	<div className="mini ui buttons right floated">						  
+						  <button onClick={this.showMore} className="ui blue basic button">Развернуть</button>
+						  <button onClick={this.editContact} className="ui green basic button">Редактировать</button>
+						  <button onClick={this.delContact} className="ui red basic button">Удалить</button>
+						</div>
+
+			    </div>	
+
+			  </div>
+
+      ); 
+  }
+
+}
+
+export default AllContact;
--- code/js/components/listContacts.js
+++ code/js/components/listContacts.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import AllContact from "./allContact.js"
 
 class ListContacts extends React.Component {	
 
@@ -20,32 +21,11 @@ class ListContacts extends React.Component {
 
   startPage() {
     return (
-      <div className="two column centered row">
 
-		    <div className="column">
+    	<div>
+    	<AllContact></AllContact>
+    	</div>   
 
-	    		<div className="ui large animated list">
-					  <div className="item">
-					    <img className="ui avatar image" src="https://semantic-ui.com/images/avatar/large/christian.jpg" />
-					    <div className="content">
-					      <a className="header">Александр Семак</a>
-					      <div className="description">+380997708164</div>
-					    </div>
-					  </div>
-					</div>
-		    </div>
-
-		    <div className="column">
-		    	
-		    	<div className="mini ui buttons right floated">						  
-					  <button onClick={this.showMore} className="ui blue basic button">Развернуть</button>
-					  <button onClick={this.editContact} className="ui green basic button">Редактировать</button>
-					  <button onClick={this.delContact} className="ui red basic button">Удалить</button>
-					</div>
-
-		    </div>	
-
-		  </div>
     );
   };
 
@@ -88,21 +68,14 @@ class ListContacts extends React.Component {
     );
   };
 
-	showMore()	{
-		alert("show");
-	};
-
-	delContact() {
-		let btnDelContact = <h1>Развернуть</h1>;
-		alert(btnDelContact);
-	};	
-
   render() {
+
       if (this.state.editContact) {
 	      return this.editPage ();
 	    } else {
 	      return this.startPage ();
 	    }
+
   }
 
 }
