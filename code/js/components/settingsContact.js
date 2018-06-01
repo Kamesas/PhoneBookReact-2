c80a52e9369644d496fa81173b6598c2e23e2969
Alexandr <semakaleksandr2014@gmail.com>
2018-06-01 15:12:42 +0300

    func show, edit & del

--- code/js/components/settingsContact.js
+++ code/js/components/settingsContact.js
@@ -2,13 +2,25 @@ import React from 'react';
 
 class SettingsContacts extends React.Component {
 
+	showMore()	{
+		alert("show");
+	};
+
+	editContact() {
+		alert("edit contact");
+	};
+
+	delContact() {
+		alert("edit contact");
+	};
+
   render() {
       return (
 
 		    		<div className="mini ui buttons right floated">						  
-						  <button className="ui blue basic button">Развернуть</button>
-						  <button className="ui green basic button">Редактировать</button>
-						  <button className="ui red basic button">Удалить</button>
+						  <button onClick={this.showMore} className="ui blue basic button">Развернуть</button>
+						  <button onClick={this.editContact} className="ui green basic button">Редактировать</button>
+						  <button onClick={this.delContact} className="ui red basic button">Удалить</button>
 						</div>
 										 
       ); 
